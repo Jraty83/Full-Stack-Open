dd7455bea24036d22e628521795ca9528861e221
jraty <jraty@student.hive.fi>
2021-02-03 13:13:41 +0200

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react'
 import Numbers from './components/Numbers'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ showFiltered, setShowFiltered ] = useState('')
+
+  const numbersToShow = showFiltered === persons
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(showFiltered.toLowerCase()))
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -31,6 +39,10 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setShowFiltered(event.target.value)
+  }
+
   const isDuplicate = () => {
     const dup = (element) => element === newName
     return (persons.map(e => e.name).some(dup))
@@ -39,6 +51,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with <input
+            value={showFiltered}
+            onChange={handleFilterChange}
+          />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addNumber}>
         <div>
           name: <input
@@ -58,7 +77,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
+        {numbersToShow.map(person =>
           <Numbers key={person.name} name={person.name} number={person.number} />)}
       </div>
     </div>
--- part2/phonebook/src/components/Numbers.js
+++ part2/phonebook/src/components/Numbers.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Numbers = ({ name,number }) => {
   return (
     <>
-      <p>{name} {number}</p>
+      <div>{name} {number}</div>
     </>
   )
 }
